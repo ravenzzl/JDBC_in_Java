c7f509ed12483258df68feb72cdb54d6002d16c2
ravenzzl <ravenzzco1@gmail.com>
2024-02-20 16:26:32 +0530

    read values

--- src/com/jdbc/ReadValuesInDB.java
+++ src/com/jdbc/ReadValuesInDB.java
@@ -3,31 +3,29 @@ package com.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-public class InsertionValuesInDB {
+public class ReadValuesInDB {
 
 	public static void main(String[] args) {
+		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con=DriverManager.getConnection("jdbc:mysql://localhost/mydb","root","root");
-			String sql="insert into account values(?,?,?,?);";
-			PreparedStatement stmt=con.prepareStatement(sql);
-			stmt.setString(1, "stalin");
-			stmt.setString(2, "Selvamani");
-			stmt.setString(3, "stalin@gmail.com");
-			stmt.setString(4, "1030");
-			int rs=stmt.executeUpdate();
-			if(rs>0) {
-				System.out.println(rs+" rows got affected");
-			}
+		    PreparedStatement stmt=con.prepareStatement("select * from mydb.account");
+		    ResultSet rs= stmt.executeQuery();
+		    while(rs.next()) {
+		    	System.out.println(rs.getString(1));
+		    	System.out.println(rs.getString(2));
+		    	System.out.println(rs.getString(3));
+		    	System.out.println(rs.getString(4));
+		    }
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
 
 	}
 
